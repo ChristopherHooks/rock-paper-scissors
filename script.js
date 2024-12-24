2e73769abdbe1df2de8372d3f4319c8dfee36e1d
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-23 20:48:12 -0600

    Create human choice, add score, play round

--- script.js
+++ script.js
@@ -7,30 +7,58 @@ function getComputerChoice() {
     let computerGuess = Math.floor(Math.random() * 3) + 1;
 
     if (computerGuess === 1) {
-
         return "rock";
     } else if (computerGuess === 2) {
-
         return "paper";
     } else {
-
         return "scissors";
     }
 
 }
 //Test getComputerChoice
-
 console.log(computerChoice);
+
 //Create variable humanChoice
+const humanChoice = getHumanChoice();
+
 //Create function getHumanChoice
-//Test getUserChoice
-//Create humanScore variable & set to 0
-//Create computerScore variable & set to 0
-//Create function playRound
 //make humanChoice case-insensitive
+function getHumanChoice() {
+    let humanGuess = prompt("Enter rock, paper, or scissors.").toLowerCase();
+    return humanGuess;
+}
+//Test getHumanChoice
+console.log(humanChoice);
+
+//Create humanScore variable & set to 0
+const humanScore = 0;
+//Create computerScore variable & set to 0
+const computerScore = 0;
+
+//Create function playRound
 //Log a winner for the round
 //Increment humanScore or computerScore
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! Both players chose ${humanChoice}`);
+        return;
+    } else if (
+        humanChoice === rock && computerChoice === scissors ||
+        humanChoice === paper && computerChoice === computerChoice === rock ||
+        humanChoice === scissors && computerChoice === paper
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        return;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+        return;
+    }
+}
 
+playRound();
+console.log(`Player: ${humanScore} Computer: ${computerScore}`);
 //Create function playGame
 //Move playRound here
 //Move scores here
